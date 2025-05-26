18b77288dc2ed4bfe5a1a3f4a55fb14a8a65a714
Aayush <aayushsingh91255@gmail.com>
2025-05-26 16:41:22 +0530

    generate Stream token (backend checkpoint )
    Begin with Frontend

---
+++ Backend/src/controller/chatController.js
@@ -0,0 +1,10 @@
+import { generateStreamToken } from "../lib/streamChat.js";
+export async function getStreamToken(req, res) {
+  try {
+    const token = generateStreamToken(req.user.id);
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log("Error in getStreamToken", error);
+    return res.status(500).json({ message: "internal server error" });
+  }
+}
---
+++ Backend/src/routes/chat.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth_middleware.js";
+import { getStreamToken } from "../controller/chatController.js";
+const router = express.Router();
+router.get("/token", protectRoute, getStreamToken);
+export default router;
