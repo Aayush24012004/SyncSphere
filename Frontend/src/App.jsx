67c61772dbcf717fdcf2cf85b6b4ad5f5d8f13e1
Aayush <aayushsingh91255@gmail.com>
2025-05-27 01:26:52 +0530

    Frontend component lib, routing, and toast setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-
+import { Route, Routes } from "react-router";
+import HomePage from "./routes/HomePage";
+import SignupPage from "./routes/SignupPage";
+import SigninPage from "./routes/SigninPage";
+import NotificationsPage from "./routes/NotificationsPage";
+import CallPage from "./routes/CallPage";
+import ChatPage from "./routes/ChatPage";
+import OnboardingPage from "./routes/OnboardingPage";
+import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
     <div className=" h-screen" data-theme="forest">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/notification" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+      </Routes>
+      <Toaster />
     </div>
   );
 };
